446d0bd59bd4b5e6cc0993d04314332c111c29d8
Sumaiya jouty <farhanajouty87@gmail.com>
2024-12-26 09:55:43 +0600

    First commit

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1, // Replace with the actual property name for the primary key
+                CouponCode = "10OFF", // Example property
+                DiscountAmount = 10, // Example property
+                MinAmount = 20,  // Example property
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2, // Replace with the actual property name for the primary key
+                CouponCode = "20OFF", // Example property
+                DiscountAmount = 20, // Example property
+                MinAmount = 40,  // Example property
+            });
+        }
+
+    }
+}
